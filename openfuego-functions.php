09bae5ba5ad3e4dc95fc76d6f6b15516f4914385
Andrew Phelps <andrew@phel.ps>
2013-05-03 14:50:35 -0400

    Moved helper functions into separate file; merged salsa into main functions file; cleaned up comments
    Signed-off-by: Andrew Phelps <andrew@phel.ps>

--- openfuego-functions.php
+++ openfuego-functions.php
@@ -1,65 +1,7 @@
-<?php
+<?php // This is the salsa right here.
 
+include(OPENFUEGO_DIR . '/openfuego-helper-functions.php');
 include(OPENFUEGO_DIR . '/openfuego-metadata-functions.php');
-
-function openfuego_echo($string) {
-	if (OPENFUEGO_DEBUG) {
-    	echo $string;
-		return TRUE;
-    } else {
-    	return FALSE;
-	}
-}
-
-function openfuego_get_dbh($db_host = OPENFUEGO_DB_HOST, $db_port = OPENFUEGO_DB_PORT, $db_name = OPENFUEGO_DB_NAME, $db_user = OPENFUEGO_DB_USER, $db_pass = OPENFUEGO_DB_PASS, $driver = OPENFUEGO_DB_DRIVER) {
-
-	$dsn = "$driver:host=$db_host;port=$db_port;dbname=$db_name";
-
-	$try = 1;
-	
-	while ($try <= 3) {	
-		try {
-			$dbh = new PDO($dsn, $db_user, $db_pass, array(
-				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8' COLLATE 'utf8_unicode_ci';",
-				PDO::ATTR_PERSISTENT => true
-			));
-			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			return $dbh;
-		}
-		
-		catch (PDOException $e) {
-			openfuego_notify('OpenFuego exception in ' . __FUNCTION__, $e);
-			sleep($try*30);
-			$try++;
-			continue;
-		}
-	}
-	return FALSE;
-}
-
-
-function openfuego_strpos_arr($haystack, $needles, $before = NULL, $after = NULL) { 
-    foreach($needles as $needle) { 
-        if(($pos = strpos($haystack, $before . $needle . $after)) !== FALSE) return $pos; 
-    }
- return FALSE;
-} 
-
-
-function openfuego_notify($subject, $body = NULL) {
-	
-	if (OPENFUEGO_DEBUG) echo $subject . "\n" . $body;
-	mail(OPENFUEGO_WEBMASTER, $subject, print_r($body, TRUE), 'From: ' . OPENFUEGO_POSTMASTER);
-	return TRUE;
-}
-
-
-function array_most_common($input) {
-	$counted = array_count_values($input);
-	arsort($counted);
-	return($counted);
-}
-
 
 function openfuego_get_universe($min_influence = 1) {
 	
@@ -97,146 +39,6 @@ function openfuego_clean_up() {
 
   return TRUE;
  }
-
-
-function openfuego_curl($url, $method = 'GET', $headers = FALSE, $limit = FALSE) {
-
-	$ch = curl_init($url);
-
-	$options = array(
-		CURLOPT_USERAGENT => OPENFUEGO_USER_AGENT,
-		CURLOPT_REFERER => OPENFUEGO_REFERER,
-		CURLOPT_CONNECTTIMEOUT => 15,
-		CURLOPT_TIMEOUT => 15,
-		CURLOPT_RETURNTRANSFER => TRUE,
-		CURLOPT_BINARYTRANSFER => TRUE,
-		CURLOPT_FOLLOWLOCATION => TRUE,
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_AUTOREFERER => TRUE,
-		CURLOPT_SSL_VERIFYPEER => FALSE,
-		CURLOPT_ENCODING => '', // blank supports all encodings
-		CURLOPT_HTTPHEADER => array('Expect:'),
-		CURLOPT_HEADER => $headers,
-		CURLOPT_NOBODY => $headers,
-	);
-		
-	curl_setopt_array($ch, $options);
-
-	if ($limit) {
-		$writefn = function($ch, $chunk) {
-			static $limit = 7000;
-	
-			static $data = '';
-		global $data; // There is probably a better way to do this.
-		
-			$len = strlen($data) + strlen($chunk);
-			if ($len >= $limit ) {
-				$data .= substr($chunk, 0, $limit-strlen($data));
-				return -1;
-			}
-		
-			$data .= $chunk;
-			return strlen($chunk);
-		};
-
-		curl_setopt($ch, CURLOPT_WRITEFUNCTION, $writefn);
-		
-		curl_exec($ch);
-		
-		global $data; // There is probably a better way to do this.
-
-		if (mb_detect_encoding($data, NULL, TRUE) == 'ASCII') {
-			$data = utf8_encode($data);
-		}
-
-		return $data;
-	}
-
-	$data = curl_exec($ch);
-	
-	if (mb_detect_encoding($data, NULL, TRUE) == 'ASCII') {
-		$data = utf8_encode($data);
-	}
-
-	return $data;
-}
-
-
-function openfuego_get_http_location($url, $max_redirects = 10) {
-
-	$useragent = OPENFUEGO_USER_AGENT;
-	$method = 'GET';
-	$headers = TRUE;
-
-	$ci = curl_init();
-
-	curl_setopt($ci, CURLOPT_USERAGENT, $useragent);
-	curl_setopt($ci, CURLOPT_REFERER, 'http://www.google.com/');
-	curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($ci, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ci, CURLOPT_FOLLOWLOCATION, TRUE);
-	curl_setopt($ci, CURLOPT_MAXREDIRS, $max_redirects);
-	curl_setopt($ci, CURLOPT_AUTOREFERER, FALSE);
-	curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ci, CURLOPT_HTTPHEADER, array('Expect:'));
-	curl_setopt($ci, CURLOPT_HEADER, $headers);
-	curl_setopt($ci, CURLOPT_NOBODY, $headers);
-	curl_setopt($ci, CURLOPT_URL, $url);
-
-	$response = curl_exec($ci);
-		
-	$info = curl_getinfo($ci);
-	curl_close ($ci);
-
-	$long_url = $info['url'];
-	
-	if (mb_detect_encoding($long_url, NULL, TRUE) == 'ASCII') {
-		$long_url = utf8_encode($long_url);
-	}
-
-	return $long_url;
-}
-
-
-function openfuego_get_canonical($url) {
-		
-	if (strpos($url, '?utm_')) {
-		$url = strstr($url, '?utm_', TRUE); 
-	}
-	
-	if (strpos($url, '&utm_')) {
-		$url = strstr($url, '&utm_', TRUE); 
-	}
-	
-	if (strpos($url, '#utm_')) {
-		$url = strstr($url, '#utm_', TRUE);
-	}
-	
-	if (strpos($url, '#utm_')) {
-		$url = strstr($url, '#utm_', TRUE);
-	}
-	
-	$url = str_replace('www10.', 'www.', $url); // NYT paywall handling	
-					
-	$source = openfuego_curl($url, 'GET', FALSE, 10000);
-
-	$doc = new DOMDocument();
-	@$doc->loadHTML($source);
-		unset($source);
-	$xpath = new DOMXpath($doc);
-		unset($doc);
-	$elms = $xpath->query("//link[@rel='canonical']");
-		unset($xpath);
-	if ($elms->length > 0 && strlen($elms->item(0)->getAttribute('href')) > 0 && substr(trim($elms->item(0)->getAttribute('href')), 0, 1) != '/') {
-// 	if canonical is specified AND if canonical href is not blank AND if canonical is not relative (no leading slash), we are good to go
-		$canonical_url = trim($elms->item(0)->getAttribute('href'));
-		return $canonical_url;
-
-	} else {
-		return $url;
-	}
-}
 
 
 function openfuego_get_items($quantity = 10, $hours = 24, $scoring = TRUE, $metadata = FALSE) {
@@ -284,7 +86,7 @@ function openfuego_get_items($quantity = 10, $hours = 24, $scoring = TRUE, $meta
 		$link_id = (int)$openfuego_item['link_id'];
 		
 		$url = $openfuego_item['url'];
-/* 		$url_encoded = urlencode($url); */
+// 	$url_encoded = urlencode($url);
 		$weighted_count = $openfuego_item['weighted_count'];
 		$multiplier = NULL;
 		$score = NULL;
@@ -301,14 +103,10 @@ function openfuego_get_items($quantity = 10, $hours = 24, $scoring = TRUE, $meta
 		elseif ($age >= ($hours/6) && $age < ($hours/2))		{ $multiplier = 1.05-$age/$hours; }
 		elseif ($age  > ($hours/2))								{ $multiplier = 1.01-$age/$hours; }
 		
-//		$multiplier = round($multiplier, 3);
-
 		$score = round($weighted_count * $multiplier);
 		
 		$openfuego_items_filtered[] = array('link_id' => $link_id, 'url' => $url, 'weighted_count' => $weighted_count, 'first_seen' => $first_seen, 'first_user' => $first_user, 'age' => $age, 'multiplier' => $multiplier, 'score' => $score);
 		
-/* 		$openfuego_items_urls[] = $url; // for metadata, might be a better way */
-
 		unset($link_id, $url, $weighted_count, $first_seen, $age, $multiplier, $score);
 	}
 	
@@ -322,10 +120,6 @@ function openfuego_get_items($quantity = 10, $hours = 24, $scoring = TRUE, $meta
 	array_multisort($score, SORT_DESC, $age, SORT_ASC, $openfuego_items_filtered);	// sort by score, then by age
 
 	unset($age, $score);
-/*
-	$openfuego_items = $openfuego_items_filtered;
-	unset($openfuego_items_filtered);
-*/
 	
 	$openfuego_items_filtered = array_slice($openfuego_items_filtered, 0, $quantity);
 
@@ -463,7 +257,7 @@ function openfuego_get_tweet($link_id) {
 					}
 					
 					else {
-					//	sleep($try);
+//						sleep($try);
 						$try++;
 						continue; // try again
 					}
@@ -476,8 +270,6 @@ function openfuego_get_tweet($link_id) {
 			}
 		}
 
-		// $tweet_to_cache = array($link_id, $id_str, $screen_name, $text, $profile_image_url);
-	
 		try {
 			$sth = $dbh->prepare("INSERT IGNORE INTO openfuego_tweets_cache (link_id, id_str, screen_name, text, profile_image_url) VALUES (:link_id, :id_str, :screen_name, :text, :profile_image_url);");
 /* 		$sth = $dbh->prepare("INSERT INTO openfuego_tweets_cache (link_id, id_str, screen_name, text, profile_image_url) VALUES (:link_id, :id_str, :screen_name, :text, :profile_image_url); UNLOCK TABLES"); */
@@ -518,8 +310,6 @@ function openfuego_update_tweet($link_id) {
 		$first_user_id = $row[1];
 
 		$search = $twitter->get("search/tweets", array('q' => $url, 'count' => 100));
-		// print_r($search); die;
-		// $search = json_decode($search, TRUE);
 		
 		if ($search['statuses']) {
 			$search_results = $search['statuses'];
@@ -578,14 +368,6 @@ function openfuego_last_updated($format = NULL) {
 }
 
 
-function openfuego_twitter_connect() {
-
-	$twitter_handle = new TwitterOAuth(OPENFUEGO_TW_CONSUMER_KEY, OPENFUEGO_TW_CONSUMER_SECRET, OPENFUEGO_TW_ACCESS_TOKEN, OPENFUEGO_TW_ACCESS_TOKEN_SECRET);
-
- return $twitter_handle;
-}
-
-
 function openfuego_populate_universe($authorities, $min_influence = 1) {
 
 	$owner_screen_name = strtolower(OPENFUEGO_TW_SCREEN_NAME);
@@ -607,11 +389,9 @@ function openfuego_populate_universe($authorities, $min_influence = 1) {
 		$universe_ids = array_merge($universe_ids, $authority_friends_ids); // append more ids to universe
 	}
 
-	$universe_ids_sorted = array_most_common($universe_ids);
+	$universe_ids_sorted = openfuego_array_most_common($universe_ids);
 
 	unset($authority_friends_ids, $owner_screen_name, $twitter, $universe_ids);
-	
-/* 	print '<pre>'; print_r($universe_ids_sorted); print '</pre>'; */
 	
 	$dbh = openfuego_get_dbh();
 	$dbh->exec("TRUNCATE TABLE openfuego_citizens;");
@@ -635,4 +415,187 @@ function openfuego_populate_universe($authorities, $min_influence = 1) {
  unset($dbh, $universe_ids_sorted);
  return TRUE;
 }
+
+
+function openfuego_expand($input_url) {
+	
+	global $openfuego_bitly_pro_domains;
+	global $openfuego_short_domains;
+
+	$url = urldecode($input_url);
+	
+	openfuego_echo("\nurl: $input_url");
+	
+	$url = urlencode($input_url);
+		
+	if (openfuego_strpos_arr($input_url, $openfuego_short_domains, '://', '/')) {
+		openfuego_echo(" (pre-defined short url) "); // instagram, vimeo, mlkshk...
+		return $input_url;
+	}
+	
+	elseif (strpos($input_url, '://' . 'youtu.be' . '/')) {
+
+		$canonical_url = 'http://www.youtube.com/watch?v=' . str_replace('http://youtu.be/', '', $input_url);
+		openfuego_echo(" (youtube)");
+		return $canonical_url;
+	}
+
+	$dbh = openfuego_get_dbh();
+
+	$input_url_encoded = urlencode($input_url);
+
+	$sth = $dbh->prepare("SELECT long_url FROM openfuego_short_links WHERE input_url = :input_url LIMIT 1");
+	$sth->bindParam('input_url', $input_url);
+	$sth->execute();
+	$cached_url = $sth->fetchColumn(0);
+
+	if ($cached_url) {	// if it exists in cache...
+		openfuego_echo(" (cached)");
+		return $cached_url;
+	}
+
+	$start = microtime(TRUE);
+
+	if (strlen($input_url) > 36): // if the URL is unshortened
+
+		$long_url = $input_url;
+//		$canonical_url = openfuego_get_canonical($input_url);
+		openfuego_echo(" (already expanded)");
+	
+	elseif (strpos($input_url, '://' . 'is.gd' . '/')):
+	
+		$long_url = openfuego_isgd_expand($input_url);
+		openfuego_echo(" (isgd)");
+		
+	elseif (strpos($input_url, '://' . 'goo.gl' . '/') && defined('OPENFUEGO_GOOGL_API_KEY') && OPENFUEGO_GOOGL_API_KEY):
+	
+		$long_url = openfuego_googl_expand($input_url);
+		openfuego_echo(" (googl)");
+
+	elseif (strpos($input_url, '://' . 'su.pr' . '/')):
+
+		$long_url = openfuego_supr_expand($input_url);
+		openfuego_echo(" (supr)");
+		
+	// list of known Bitly Pro domains specified in openfuego-config
+	elseif (openfuego_strpos_arr($input_url, $openfuego_bitly_pro_domains, '://', '/') && defined('OPENFUEGO_BITLY_API_KEY') && OPENFUEGO_BITLY_API_KEY):
+	
+		$long_url = openfuego_bitly_expand($input_url);
+	//	$long_url = openfuego_get_http_location($long_url); // to deal with trib.al urls
+		openfuego_echo(" (bitly)");
+		
+	else:
+		$long_url = openfuego_get_http_location($input_url);
+		openfuego_echo(" (headers)");
+	
+	endif;
+	
+	// done looping through expansion options. now, do we have a canonical_url?
+
+	if ($long_url) {
+		$canonical_url = openfuego_get_canonical($long_url);
+		$canonical_url = rtrim($canonical_url, '/');
+		$canonical_url = str_replace('www10.', 'www.', $canonical_url);
+
+		$output_url = $canonical_url ? $canonical_url : $long_url;
+
+		try {
+			$sth = $dbh->prepare("INSERT INTO openfuego_short_links (input_url, long_url) VALUES (:input_url, :output_url)");
+			$sth->bindParam(':input_url', $input_url);
+			$sth->bindParam(':output_url', $output_url);
+			$sth->execute();
+			
+			return $output_url;
+
+		} catch (PDOException $e) {
+			openfuego_notify('PDO exception in ' . __FUNCTION__ . ', ' . date('Y-m-d H:i:s'), $e);
+			return FALSE;
+		}
+
+	} else {
+		return FALSE;
+	}
+}
+
+function openfuego_process_urls($status) {
+		
+	$urls = $status['entities']['urls'];
+
+	foreach($urls as $url) {
+
+		$expanded_url = $url['expanded_url'];
+
+		$output_url = openfuego_expand($expanded_url); // sometimes "expanded url" returned by t.co service is a bitly link, etc.
+
+		$first_seen = $status['created_at'];
+		$first_seen = strtotime($first_seen);
+		$first_seen = date('Y-m-d H:i:s', $first_seen);
+
+		$first_tweet = $status['id_str'];
+
+		$first_user = $status['user']['screen_name'];
+
+		$first_user_id = $status['user']['id_str'];
+
+		$weighted_count = openfuego_get_influence($first_user_id);
+		
+		try { // should reconfigure this to open only on db connection
+			$dbh = openfuego_get_dbh();
+			$sth = $dbh->prepare("INSERT INTO openfuego_links (url, first_seen, first_tweet, first_user, first_user_id, weighted_count, count, last_seen) VALUES (:url, :first_seen, :first_tweet, :first_user, :first_user_id, :weighted_count, 1, :first_seen) ON DUPLICATE KEY UPDATE weighted_count = weighted_count + VALUES(weighted_count), count = count + 1, last_seen = VALUES(last_seen);");
+			$sth->bindParam('url', $output_url);
+			$sth->bindParam('first_seen', $first_seen);
+			$sth->bindParam('first_tweet', $first_tweet);
+			$sth->bindParam('first_user', $first_user);
+			$sth->bindParam('first_user_id', $first_user_id);
+			$sth->bindParam('weighted_count', $weighted_count);
+			$sth->execute();
+		} catch (PDOException $e) {
+			openfuego_notify('PDO exception in ' . __FUNCTION__ . ', ' . date('Y-m-d H:i:s'), $e);
+			continue; // on to the next url
+		}
+
+		unset($expanded_url, $output_url, $first_seen, $first_tweet, $first_user, $first_user_id, $weighted_count);
+	}		
+}
+
+
+function openfuego_get_influence($user_id_str) {
+
+	try {
+		$dbh = openfuego_get_dbh();
+		$sql = "SELECT influence FROM openfuego_citizens WHERE user_id = :user_id LIMIT 1;";
+		$sth = $dbh->prepare($sql);
+		$sth->bindParam('user_id', $user_id_str);
+		$sth->execute();
+		
+		$influence = $sth->fetchColumn(0);
+		
+		return $influence;
+
+	} catch (PDOException $e) {
+		openfuego_notify('PDO exception retrieving influence', $e);
+		return FALSE;
+	}
+}
+
+
+function openfuego_is_citizen($user_id_str) {
+
+	try {
+		$dbh = openfuego_get_dbh();
+		$sth = $dbh->prepare("SELECT user_id FROM openfuego_citizens WHERE user_id = :user_id LIMIT 1;");
+		$sth->bindParam('user_id', $user_id_str);
+		$sth->execute();
+		
+		if ($sth->fetchColumn(0))
+			return TRUE;
+		else
+			return FALSE;
+
+	} catch (PDOException $e) {
+		openfuego_notify('PDO exception checking for citizenry', $e);
+		return FALSE;
+	}
+}
+
 ?>
