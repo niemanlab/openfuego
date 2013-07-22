c5c0d18f30f6f1a3f502d048cbf29d3b2222d130
Andrew Phelps <andrew@phel.ps>
2013-07-22 00:33:07 -0400

    Improve example documentation
    Signed-off-by: Andrew Phelps <andrew@phel.ps>

--- examples/getItems.php
+++ examples/getItems.php
@@ -1,6 +1,22 @@
 <?php
-require_once(__DIR__ . '../../init.php');
-$fuego = new \OpenFuego\app\Getter\Getter;
+
+/**
+  * The Getter object has one method:
+  *
+  * getItems($quantity, $hours, $scoring, $metadata)
+  *
+  * $quantity (int): Number of links desired. Default 20.
+  * $hours (int): How far back to look for links. Default 24.
+  * $scoring (bool): TRUE to employ  "freshness vs. quality" algorithm
+  *   or FALSE to simply return most frequently tweeted links. Default TRUE.
+  * $metadata (bool): TRUE to hydrate URLs with Embed.ly metadata.
+  *   An API key must be set in config.php. Default FALSE.
+ */
+ 
+require(__DIR__ . '../../init.php');
+use OpenFuego\app\Getter as Getter;
+
+$fuego = new Getter();
 $items = $fuego->getItems(20, 24, TRUE, TRUE); // quantity, hours, scoring, metadata
 
 print '<pre>';
